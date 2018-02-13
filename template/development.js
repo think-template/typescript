092d85d720b17abee851c9b18da6927785a54e15
lizheming <secretlzm007@gmail.com>
2018-02-13 22:26:53 +0800

    add app_path config

--- template/development.js
+++ template/development.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const Application = require('thinkjs');
 const typescript = require('think-typescript');
 const watcher = require('think-watcher');
 const notifier = require('node-notifier');
 const instance = new Application({
   ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'app'),
   watcher: watcher,
   transpiler: [typescript, {
     compilerOptions: {
--- template/production.js
+++ template/production.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const Application = require('thinkjs');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'app'),
   proxy: true, // use proxy
   env: 'production'
 });
